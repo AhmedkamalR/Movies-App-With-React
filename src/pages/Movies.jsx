8ace3fda7b472ba962a8a423a2ead3bee87a3d9b
[AhmedkamalR] <abokamal01012@gmail.com>
2022-03-31 19:34:18 +0200

    Fix some issues

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../components/network/axiosConfig";
 import MovieCard from "../components/MovieCard/MovieCard";
+import {  Button, Container } from "react-bootstrap";
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
+  const [page, setpage] = useState(1);
   useEffect(() => {
     axiosInstance
-      .get("3/movie/popular", {
-        params: {
-          api_key: "b7fcc38e46bc53000e574978b2731a2e",
-        },
-      })
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=b7fcc38e46bc53000e574978b2731a2e&page=${page}`)
       .then((res) => setMovies(res.data.results))
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
   const searching = (e) => {
     console.log("is searching");
     axiosInstance
@@ -28,6 +26,16 @@ export default function Movies() {
       })
       .catch((err) => console.log(err));
   };
+  function changePage(str){
+    if(str === "next"){
+       setpage((page+1));
+    }
+    if(str === "previous" && page > 1){
+      setpage((page-1))
+    }
+    
+  }
+
   return (
     <>
       <input className="col-4 md-3 m-3  " onChange={searching} type="text"></input>
@@ -35,6 +43,10 @@ export default function Movies() {
                 Search
               </button>
       <li className="page-item"></li>
+      <Container className="d-flex justify-content-around">
+      <Button style={{width:"100px"}} onClick={()=>{changePage("previous")}}  variant="outline-danger">previous</Button>
+          <Button style={{width:"100px"}} onClick={()=>{changePage("next")}}  variant="outline-primary">Next</Button>
+        </Container>
       <div className="row row-cols-1 row-cols-md-3 g-4 p-4">
         {movies.map((Movie) => {
           return (
@@ -44,6 +56,7 @@ export default function Movies() {
           );
         })}
       </div>
+
     </>
   );
 }
