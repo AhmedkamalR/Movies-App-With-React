602f200c35f1d1b341aa2b2b41fc8825bebe44ff
[AhmedkamalR] <abokamal01012@gmail.com>
2022-04-02 13:08:11 +0200

    New Edit and organize code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import NotFound from "./pages/NotFound.jsx";
 import Navbar from "./components/Navbar/Navbar";
-import Movies from "./pages/Movies";
-import MovieDetails from "./pages/MovieDetails";
-import Favourite from "./components/Favourites/Favourite";
-// import Home from './components/Home/Home';
-// import Search from './components/search/Search.jsx';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+import Loader from './components/Loader/Loader';
 
+const Login = React.lazy(() => import("./components/Login/Login"));
+const Register = React.lazy(() =>
+  import("./components/Register/Register")
+);
+const NotFound = React.lazy(() => import("./pages/NotFound.jsx"));
+const Movies = React.lazy(() => import("./pages/Movies"));
+const MovieDetails = React.lazy(() => import("./pages/MovieDetails"));
+const Favourite = React.lazy(() => import("./components/Favourites/Favourite"));
 
 function App() {
   return (
     <div className="App ">
       <BrowserRouter>
         <Navbar />
+        <Suspense fallback={<Loader />}>
         <Switch>
           <Route path={"/"} exact component={Login} />
           {/* <Route path={"/"} exact component={Search} /> */}
           <Route path="/Movies" exact component={Movies} />
           <Route path="/Movies/:id" exact component={MovieDetails} />
           <Route path="/Favourites" exact component={Favourite} />
-
           <Route path={"/Login"} exact component={Login} />
           <Route path={"/Register"} exact component={Register} />
           <Route path={"*"} component={NotFound} />
         </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
